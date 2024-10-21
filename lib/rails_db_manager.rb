69446ac8d0dbe9ee2a7174ffb3769f6bd08a5551
Ignacio Sfeir <ignacio.sfeir@renuo.ch>
2024-10-21 17:58:18 +0200

    bundle gem

---
+++ lib/rails_db_manager.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_db_manager/version"
+
+module RailsDbManager
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rails_db_manager_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RailsDbManager do
+  it "has a version number" do
+    expect(RailsDbManager::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
