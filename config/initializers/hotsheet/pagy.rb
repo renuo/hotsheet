738d308f2d076ae2681069a6fe141596a64c0166
Chris <76159444+hunchr@users.noreply.github.com>
2024-11-04 17:16:12 +0100

    Implement feedback

--- config/initializers/hotsheet/pagy.rb
+++ config/initializers/hotsheet/pagy.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "pagy"
-require "pagy/extras/bootstrap"
 
 # Pagy Variables (https://ddnexus.github.io/pagy/docs/api/pagy#variables)
 Pagy::DEFAULT[:limit] = 100
