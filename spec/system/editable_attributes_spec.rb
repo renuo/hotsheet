7ee859fbc3dccb39f281250a645a067a97b96891
simon <mail@simonisler.ch>
2024-11-15 11:39:07 +0100

    Fix flaky spec

--- spec/system/editable_attributes_spec.rb
+++ spec/system/editable_attributes_spec.rb
@@ -7,11 +7,16 @@ RSpec.describe "editable attributes", :js do
 
   describe "update strings" do
     before do
+      Hotsheet.configure do |config|
+        config.model :Author do |model|
+          model.included_attributes = %i[name birthdate]
+        end
+      end
+      Hotsheet.instance_variable_set(:@editable_attributes_for, nil) # reset the memoized editable attributes
+
       visit "/hotsheet"
       click_link "Author"
 
-      wait_for_turbo
-      expect(page).to have_content("Stephen") # rubocop:disable RSpec/ExpectInHook
       find(".readonly-attribute", text: "Stephen").click
       fill_in "author_name", with: "King"
     end
