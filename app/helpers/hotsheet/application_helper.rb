c474bd23e2c6b3171dae8d40e9699677b38f9cb9
Chris <76159444+hunchr@users.noreply.github.com>
2025-04-17 12:13:02 +0000

    Remove dependencies and update gemspec

--- app/helpers/hotsheet/application_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Hotsheet
-  module ApplicationHelper
-    include Pagy::Frontend
-  end
-end
--- config/initializers/hotsheet/pagy.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "pagy"
-
-# Pagy Variables (https://ddnexus.github.io/pagy/docs/api/pagy#variables)
-Pagy::DEFAULT[:limit] = 100
-Pagy::DEFAULT.freeze
