80de3b81c7985895696c273004a7ef045a8a85f7
Chris <76159444+hunchr@users.noreply.github.com>
2025-05-09 15:20:29 +0000

    Change hash syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Hotsheet::Engine.routes.draw do
   Hotsheet.sheets.each_key do |sheet_name|
-    resources sheet_name, only: %i[index update], controller: :sheets, sheet_name:
+    resources sheet_name, only: %i[index update], controller: :sheets, sheet_name: sheet_name
   end
 
   root "sheets#root"
