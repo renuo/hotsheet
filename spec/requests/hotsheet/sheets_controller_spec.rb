4cc415527dee6dbd392d81160acc61f26668bb66
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-27 11:58:11 +0000

    Rebase

--- spec/requests/hotsheet/sheets_controller_spec.rb
+++ spec/requests/hotsheet/sheets_controller_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Hotsheet::SheetsController do
       it "shows an error page" do
         get "/hotsheet/authors"
         expect(response).to be_not_found
-        expect(response.body).to include I18n.t("hotsheet.errors.not_found")
+        expect(response.body).to include Hotsheet.t("error_not_found")
       end
     end
   end
