bc823ce488296403e6a3e48329cdd4987c86033e
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-27 08:26:29 +0000

    Update docs

--- lib/generators/templates/hotsheet.rb
+++ lib/generators/templates/hotsheet.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 # Configure the models to be used by Hotsheet.
-# See https://github.com/renuo/hotsheet?tab=readme-ov-file#usage.
-# The ID is included by default. It is always the first column.
+# See https://github.com/renuo/hotsheet/blob/main/README.md#usage.
 
 Hotsheet.configure do
+  # Configure the visible and editable columns for each model.
+  # The ID is included as the first column by default.
+
   # sheet :User do
   #   column :name
-  #   column :birthdate, editable: false
+  #   column :email, editable: false
+  #   column :birthdate, editable: -> { rand > 0.5 }
   # end
+
+  # Leave the block out to include all database columns.
+
+  # sheet :Post
 end
