c5356354e8ff1bd4525ab2ec6fc758097b347728
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-28 07:46:53 +0000

    Update docs

--- lib/generators/templates/hotsheet.rb
+++ lib/generators/templates/hotsheet.rb
@@ -6,8 +6,10 @@
 Hotsheet.configure do
   # Configure the visible and editable columns for each model.
   # The ID is included as the first column by default.
+  # The number of rows displayed can be configured using the `per` option,
+  # which defaults to 100.
 
-  # sheet :User do
+  # sheet :User, per: 10 do
   #   column :name
   #   column :email, editable: false
   #   column :birthdate, editable: -> { rand > 0.5 }
