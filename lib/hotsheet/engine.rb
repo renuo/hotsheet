3e5568be02b85f89216bbfbaee8680d0d1809fe0
Chris <76159444+hunchr@users.noreply.github.com>
2025-06-30 08:20:04 +0200

    Support import maps (#48)

--- lib/hotsheet/engine.rb
+++ lib/hotsheet/engine.rb
@@ -3,8 +3,8 @@
 class Hotsheet::Engine < Rails::Engine
   isolate_namespace Hotsheet
 
-  if config.respond_to? :assets
-    config.assets.paths << Hotsheet::Engine.root.join("app/assets")
-    config.assets.precompile << %w[hotsheet.css hotsheet.js]
+  initializer "hotsheet.assets" do |app|
+    app.config.assets.paths << Hotsheet::Engine.root.join("app/assets")
+    app.config.assets.precompile += %w[hotsheet.css hotsheet.js]
   end
 end
